6f4bbb017fdca0a9edeb54764d9911c6dd152e09
Jaan Jänesmäe <git@janesmae.com>
2024-05-31 22:39:27 +0300

    Adding localstorage methods

---
+++ assets/js/localstorage.js
@@ -0,0 +1,42 @@
+import { updatePage } from "./logs.js";
+
+/** Methods to store and restore field values for EDI header */
+const inputSaveAndRestore = function (e) {
+    if (localStorage[e.id]) e.value = localStorage[e.id];
+    e.addEventListener('input', function () {
+        localStorage[e.id] = e.value;
+        updatePage();
+    });
+};
+const selectSaveAndRestore = function (e) {
+    e.addEventListener('change', function () {
+        localStorage[e.id] = e.value;
+        updatePage();
+    });
+    if (localStorage[e.id]) {
+        let opts = e.options;
+        for (let opt, j = 0; opt = opts[j]; j++) {
+            if (opt.value === localStorage[e.id]) {
+                e.selectedIndex = j;
+                break;
+            }
+        }
+    }
+};
+const checkboxSaveAndRestore = function (e) {
+    if (localStorage[e.id] && (localStorage[e.id] == 1)) e.checked = true;
+    e.addEventListener('change', function () {
+        if(e.checked){
+            localStorage[e.id] = 1;
+        } else {
+            localStorage[e.id] = 0;
+        }
+        updatePage();
+    });
+};
+
+/** Run all the scrips */
+document.querySelectorAll('#edi_header input:not([type="checkbox"])').forEach(x => inputSaveAndRestore(x));
+document.querySelectorAll('#edi_header select').forEach(x => selectSaveAndRestore(x));
+document.querySelectorAll('.contest select').forEach(x => selectSaveAndRestore(x));
+document.querySelectorAll('input[type="checkbox"]').forEach(x => checkboxSaveAndRestore(x));
